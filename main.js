b5c13e8edb823a85bcc349cd0aa1d15e678d475f
Nitish <nitish@dexecure.com>
2018-12-18 22:53:53 +0800

    test commit

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-var post = {};
-
-var handler = {
-    post: function () {
-        var addPost = document.getElementById('submit');
-        console.log (addPost);
-        alert('hello');
-    }
+function submit () {
+    var addPost = document.getElementById('submit');
+    alert('hello');
 }
